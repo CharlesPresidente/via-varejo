31d0b1812e0d1ad656497f11f2c8f84c21258919
Charles Silva Batista dos Santos <charles.santos@MJV.net>
2019-04-24 13:39:11 -0300

    iniciar controle-financeiro

--- src/app/components/extrato-transacoes/extrato-transacoes.component.ts
+++ src/app/components/extrato-transacoes/extrato-transacoes.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-extrato-transacoes',
   templateUrl: './extrato-transacoes.component.html',
   styleUrls: ['./extrato-transacoes.component.scss']
 })
+
 export class ExtratoTransacoesComponent implements OnInit {
+
+  @Input() extratoTransacoesInput;
+
+  public extratoObject: any = [
+    {
+      'nome': 'Sapato',
+      'tipo': 'venda',
+      'valor': 9.09
+    },
+    {
+      'nome': 'Monitor',
+      'tipo': 'compra',
+      'valor': 82.00
+    },
+  ];
+  public total = 0;
 
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < this.extratoObject.length; i++) {
+      this.total += parseFloat(this.extratoObject[i].valor);
+    }
   }
-
 }
--- src/app/components/nova-transacao/nova-transacao.component.ts
+++ src/app/components/nova-transacao/nova-transacao.component.ts
@@ -1,35 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nova-transacao',
   templateUrl: './nova-transacao.component.html',
   styleUrls: ['./nova-transacao.component.scss']
 })
+
 export class NovaTransacaoComponent implements OnInit {
 
+  @Output() novaTransacaoOutput = new EventEmitter();
+
+  public items: FormArray;
   public tipoObject: any = ['Compra', 'Venda'];
   public transacaoForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private form: FormBuilder) { }
 
   ngOnInit() {
-
-    this.transacaoForm = this.formBuilder.group({
-      tipo: ['Compra'],
-      nome: ['', Validators.required],
-      valor: ['R$ 0,00', Validators.required]
-    })
+    this.transacaoForm = this.form.group({
+      items: this.form.array([this.createItem()])
+    });
   }
+
+  createItem(): FormGroup {
+    return this.form.group({
+      tipo: ['', Validators.required],
+      nome: ['', Validators.required],
+      valor: ['', Validators.required]
+    });
+  }
+
 
   get f() { return this.transacaoForm.controls; }
 
   registrar() {
     if (this.transacaoForm.invalid) {
-      console.log('Registrar: Erro');
+      alert("Favor preencher todos os campos!");
       return
     }
 
-    console.log('Registrar: ', this.transacaoForm.controls);
+    this.items = this.transacaoForm.get('items') as FormArray;
+    this.items.push(this.createItem());
+
+    this.novaTransacaoOutput.emit();
   }
 }
