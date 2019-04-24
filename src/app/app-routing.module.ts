6b7e5d21d6ab62eb3f5635b01508ffce8ca07d92
Charles Silva Batista dos Santos <charles.santos@MJV.net>
2019-04-24 10:03:48 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ControleFinanceiroComponent } from './pages/controle-financeiro/controle-financeiro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: ControleFinanceiroComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
