eab6bfe19f981dae49b2d79d44ddb4879431b2ed
Charles Silva Batista dos Santos <charles.santos@MJV.net>
2019-04-24 10:04:58 -0300

    importar componentes

--- src/app/pages/controle-financeiro/controle-financeiro.module.ts
+++ src/app/pages/controle-financeiro/controle-financeiro.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ControleFinanceiroComponent } from './controle-financeiro.component';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   declarations: [ControleFinanceiroComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ]
 })
 export class ControleFinanceiroModule { }
